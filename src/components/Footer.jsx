eb039234187d2b6bb5e18fb778224078fc42d0dc
pradyumnsingh13 <pradyumnsingh0208@gmail.com>
2024-02-20 23:46:55 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+    </div>
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://youtube.com/@pradyumnsingh1503?si=vE-nblvqgNcl_gqq" target={"blank"}>Youtube</a>
+            <a href="https://www.instagram.com/_whoispradyumn?igsh=MWtxcmZ4N2hwYWNkYg==" target={"blank"}>Instagram</a> 
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
